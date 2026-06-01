eaad11dceeb8c7dba9b76e87e176caa52b2b3dd4
john kamau <jane2240k@gmail.com>
2026-06-01 12:08:21 +0300

    Add cicd-pipeline-key for SSH access

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ resource "aws_security_group" "web" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
+  key_name               = "cicd-pipeline-key"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
 
